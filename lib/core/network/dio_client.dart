c3bfe3fea1302ae29a7d9a86feff42430b5af0b4
adrisabik <adrisabik@gmail.com>
2026-01-08 16:49:57 +0700

    feat(network): integrate interceptors in DioClient

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -1,11 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_mvvm_boilerplate/core/config/env_config.dart';
 import 'package:flutter_mvvm_boilerplate/core/constants/app_durations.dart';
+import 'package:flutter_mvvm_boilerplate/core/network/interceptors/auth_interceptor.dart';
+import 'package:flutter_mvvm_boilerplate/core/network/interceptors/error_interceptor.dart';
+import 'package:flutter_mvvm_boilerplate/data/datasources/local/secure_storage_service.dart';
 
 /// Dio HTTP client configuration.
 class DioClient {
-
-  DioClient() {
+  DioClient(this._storageService) {
     _dio = Dio(
       BaseOptions(
         baseUrl: EnvConfig.current.baseUrl,
@@ -21,11 +23,20 @@ class DioClient {
 
     _setupInterceptors();
   }
+
+  final SecureStorageService _storageService;
   late final Dio _dio;
 
   Dio get dio => _dio;
 
   void _setupInterceptors() {
+    // Order matters: Auth -> Error -> Logging
+    // Auth interceptor for token management
+    _dio.interceptors.add(AuthInterceptor(_storageService, _dio));
+
+    // Error interceptor for global error handling
+    _dio.interceptors.add(ErrorInterceptor());
+
     // Logging interceptor (only in dev)
     if (EnvConfig.current.enableLogging) {
       _dio.interceptors.add(
@@ -36,9 +47,6 @@ class DioClient {
         ),
       );
     }
-
-    // TODO: Add AuthInterceptor for token management
-    // TODO: Add ErrorInterceptor for global error handling
   }
 
   /// Sets the authorization header with the provided token.
